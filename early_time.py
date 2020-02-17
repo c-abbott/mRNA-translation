a5345b1f830aeeb2604c19e8853f468fa2fe0d84
c-abbott <callum.abbott@outlook.com>
2020-02-17 19:54:53 +0000

    Fixed density plot.

--- early_time.py
+++ early_time.py
@@ -25,7 +25,7 @@ def main():
         T = int(items[4])        # Upper time limit.
         n_traj = int(items[5])   # Number of trajectories.
         n_meas = int(items[6])   # Number of measurements.
-    omegas = np.ones(L)*1.0      # Transition Rates.
+    omegas = np.ones(L)*5.0      # Transition Rates.
     omegas[0] = 0                # Nothing will occupy first site.
     omegas[-1] = beta            # Detatch at final site.
     # Setting time domains for observables.
@@ -35,24 +35,22 @@ def main():
     densities = []
 
     for i in range(n_traj):
-        print(i)
-        # Create new instance of the simulation for every trajectory.
-        simulation = ProteinSynthesis(length=l, size=L, alpha=alpha, omegas=omegas)
+        # Data storage.
+        traj_densities = []
         # Time initiation.
         t_old = 0
         t_new = 0
-        # Data storage.
-        traj_densities = []
+        print(i)
+        # Create new instance of the simulation for every trajectory.
+        simulation = ProteinSynthesis(length=l, size=L, alpha=alpha, omegas=omegas)
         # Begin trajectory.
-        while t_new < T:
+        while t_new <= T:
             # Collect all possible moves.
             R = simulation.get_R()
             # Sample random time.
             t_new += simulation.get_random_time(R)
-            #print(t_new)
             # Count time ticks.
-            ticks = simulation.count_ticks(t_new - t_old, dt)
-            #print(ticks)
+            ticks = simulation.count_ticks((t_new - t_old), dt)
             for j in range(ticks):
                 traj_densities.append(simulation.get_occupation_number() / (simulation.size - 1))
             # Update t_old
@@ -61,13 +59,16 @@ def main():
             index = simulation.get_transition(R)
             # Update simulation - ribosome hops.
             simulation.update(index)
-        
-        densities.append(np.array(traj_densities)[:n_meas-(n_meas//10)])
+
+        for k in range(n_meas - len(traj_densities)):
+            traj_densities.append(simulation.get_occupation_number() / (simulation.size - 1))
+        densities.append(np.array(traj_densities[:n_meas]))
+    print(i)
     
     densities_array = 1 / n_traj * np.mean(densities, axis = 0)
     print(densities_array)
 
-    plt.plot(measure_times[:n_meas-(n_meas//10)], densities_array)
+    plt.plot(measure_times, densities_array)
     plt.show()
 
 main()
