499beb9d07d49c904f4547c13658e4f0fe3bde9d
c-abbott <callum.abbott@outlook.com>
2020-02-18 11:22:52 +0000

    Implented functionality to import elongation rates.

--- early_time.py
+++ early_time.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 
 def main():
     # Taking in arguments.
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Wrong number of arguments.")
-        print("Usage: " + sys.argv[0] + " <parameters file>")
+        print("Usage: " + sys.argv[0] +
+              " <parameters file>" + "<translation rates>")
         quit()
     else:
         infile_parameters = sys.argv[1]
+        trans_params = sys.argv[2]
 
     # Open input file and assinging parameters.
     with open(infile_parameters, "r") as input_file:
@@ -20,15 +22,21 @@ def main():
         items = line.split(", ")
 
         l = int(items[0])        # Ribosome length.
-        L = int(items[1])        # Lattice length.
-        alpha = float(items[2])  # Initiation rate.
-        beta = float(items[3])   # Detach rate.
-        T = int(items[4])        # Upper time limit.
-        n_traj = int(items[5])   # Number of trajectories.
-        n_meas = int(items[6])   # Number of measurements.
-    omegas = np.ones(L)*5.0      # Transition Rates.
+        alpha = float(items[1])  # Initiation rate.
+        beta = float(items[2])   # Detach rate.
+        T = int(items[3])        # Upper time limit.
+        n_traj = int(items[4])   # Number of trajectories.
+        n_meas = int(items[5])  # Number of measurements.
+    
+    # Initialising elongation rates.
+    with open(trans_params, "r") as f:
+        elong_omegas = np.array([float(line.split()[1]) for line in f])
+    omegas = np.zeros(elong_omegas.size + 2)
+    omegas[1:-1] = elong_omegas  # Adding elongation rates.
     omegas[0] = 0                # Nothing will occupy first site.
-    omegas[-1] = beta            # Detatch at final site.
+    omegas[-1] = beta            # Termination rate.
+    L = int(omegas.size)         # Initialising length of mRNA.
+    
     # Setting time domains for observables.
     dt = T / n_meas
     measure_times = np.arange(0, T, dt)
@@ -68,14 +76,15 @@ def main():
         for k in range(n_meas - len(traj_densities)):
             traj_densities.append(
                 simulation.get_occupation_number() / (simulation.size - 1))
+            
         # Store each trajectory.
         densities.append(np.array(traj_densities[:n_meas]))
+
     # Compute average over all trajectories.
     densities_array = 1 / n_traj * np.mean(densities, axis=0)
 
-    plt.plot(measure_times, densities_array)
-    plt.show()
-
+    # Plotting.
+    simulation.plot_density(measure_times, densities_array)
 
 main()
         
