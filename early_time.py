f49072874532da9c16eb1910c14e33b9aaddf862
c-abbott <callum.abbott@outlook.com>
2020-02-22 11:22:58 +0000

    Refined tick finding method.

--- early_time.py
+++ early_time.py
@@ -25,9 +25,9 @@ def main():
         trans_params = sys.argv[2]
 
     # Open input file and assinging parameters.
-    with open(simul_parameters, "r") as input_file:
+    with open(simul_parameters, "r") as f:
         # Read the lines of the input data file.
-        line = input_file.readline()
+        line = f.readline()
         items = line.split(", ")
 
         l = int(items[0])        # Ribosome length.
@@ -41,75 +41,76 @@ def main():
         elong_omegas = np.array([float(line.split()[1]) for line in f])
     omegas = np.zeros(elong_omegas.size + 1)
     omegas[1:] = elong_omegas    # Adding elongation rates.
-    omegas[0] = 0                # Nothing will occupy first site.
+    omegas[0] = alpha            # Initiation rate.
     L = int(omegas.size)         # Initialising length of mRNA.
-    
+
     # Setting time domains for observables.
     dt = T / n_meas
     measure_times = np.arange(0, T, dt)
-    # Data storage.
-    densities = []
+    # Density data storage.
+    densities = np.zeros(measure_times.size)
     # Tagged ribosome times to complete synthesis of first peptide.
     tagged_times = []
 
     # Simulations begin.
     for i in range(n_traj):
-        # Data storage.
-        traj_densities = []
+        # Density data storage.
+        traj_densities = np.zeros(measure_times.size)
         # Tracking first ribosome.
         ribosome_pos = 1
         check = False
         # Time initiation.
         t_old = 0
         t_new = 0
+        # Tick finding
+        k1 = 0
+        k2 = 0
         print(i)
         # Create new instance of the simulation for every trajectory.
         simulation = ProteinSynthesis(
             length=l, size=L, alpha=alpha, omegas=omegas)
-
         # Begin trajectory.
-        while t_new <= T:  
+        while t_new <= T:
+            # Get k1.
+            k1 = simulation.get_k1(t_old, dt)
             # Collect all possible moves.
             R = simulation.get_R()
             # Sample random time.
             t_new += simulation.get_random_time(R)
-            # Count time ticks.
-            ticks = simulation.count_ticks((t_new - t_old), dt)
-            for j in range(ticks):
-                traj_densities.append(
-                    simulation.get_occupation_number())
+            # Get k2.
+            k2 = simulation.get_k2(t_new, dt)
+            # Final loop check.
+            if t_new > T:
+                k2 = n_meas
+            # Setting values.
+            traj_densities[k1:k2+1] = simulation.get_occupation_number()
             # Update t_old
             t_old = t_new
             # Choose which ribosome moves.
             index = simulation.get_transition(R)
             # Update simulation - ribosome hops.
             simulation.update(index)
-
+            # Tracking initial ribosome position.
             if check == False:
                 if (index+1) == ribosome_pos:
                     ribosome_pos += 1
                 elif ribosome_pos > simulation.size:
                     tagged_times.append(t_new)
                     check = True
-        
-        # Add on missed ticks.
-        for k in range(n_meas - len(traj_densities)):
-            traj_densities.append(
-                simulation.get_occupation_number())
-            
-        # Store each trajectory.
-        densities.append(np.array(traj_densities[:n_meas]))
+        # Collect data for each trajectory. 
+        densities += traj_densities
+    
+    # Compute actual density.
+    densities = densities / (n_traj * (L - 1))
 
     # Comparing analytical and experimental results
     avg_t1 = np.mean(np.array(tagged_times))
     print(avg_t1)
     exact_t1 = np.sum(np.reciprocal(omegas[1:]))
     print(exact_t1)
-
-    # Compute average over all trajectories.
-    densities_array = 1 / (simulation.size - 1) * np.mean(densities, axis=0)
+    
     # Plotting.
-    simulation.plot_density(measure_times, densities_array, avg_t1)
+    simulation.plot_density(measure_times, densities, avg_t1)
 
 main()
         
