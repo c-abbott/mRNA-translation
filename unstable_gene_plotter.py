d73632b838f2f9926da7e30b016de120612357cd
c-abbott <callum.abbott@outlook.com>
2020-03-08 13:10:32 +0000

    More plotting.

--- unstable_gene_plotter.py
+++ unstable_gene_plotter.py
@@ -27,7 +27,7 @@ def plotter(dict, gradient, intercept, pear_r, p_val, x_label, y_label, xaxis_la
     plt.plot(x_data, gradient*x_data + intercept, 'r',
              label="Fit |%6s, r = %6.2e, p = %6.2e" % ('red', pear_r, p_val))
     plt.legend()
-    plt.savefig(file_name)
+    plt.savefig('unstable_dat/' + file_name)
     plt.show()
 
 def main():
@@ -63,7 +63,6 @@ def main():
         gene_data["label"].append(label)
 
     # Plotting.
-
     # Translation Time vs. mRNA Half-Life.
     m_1, c_1 = get_fit(gene_data, "t_1/2", "T1_exp")
     r_1, p_1 = get_correlation(gene_data, "t_1/2", "T1_exp")
@@ -75,6 +74,52 @@ def main():
     m_2, c_2 = get_fit(gene_data, "t_1/2", "lambda")
     r_2, p_2 = get_correlation(gene_data, "t_1/2", "lambda")
     plotter(gene_data, m_2, c_2, r_2, p_2, "t_1/2",
-            "lambda", 'mRNA Half-Life (s)', 'Initiation Time ' + '\lambda ' +'(s)' , 'Initiation Time vs. mRNA Half-Life',
+            "lambda", 'mRNA Half-Life (s)', 'Initiation Time ' +
+            r'[$\lambda$] ' + '(s)', 'Initiation Time vs. mRNA Half-Life',
             "unstable_lambda_half.png")
+
+    # CDS vs. Half-life
+    m_3, c_3 = get_fit(gene_data, "t_1/2", "CDS")
+    r_3, p_3 = get_correlation(gene_data, "t_1/2", "CDS")
+    plotter(gene_data, m_3, c_3, r_3, p_3, "t_1/2",
+            "CDS", 'mRNA Half-Life (s)', 'CDS Length (codons)', 'CDS vs. mRNA Half-Life',
+            "unstable_cds_half.png")
+    
+    # Initiation Rate vs. Half-life.
+    m_4, c_4 = get_fit(gene_data, "t_1/2", "alpha")
+    r_4, p_4 = get_correlation(gene_data, "t_1/2", "alpha")
+    plotter(gene_data, m_4, c_4, r_4, p_4, "t_1/2",
+            "alpha", 'mRNA Half-Life (s)', r"[$\alpha$] $s^{-1}$", 'Initiation Rate vs. mRNA Half-Life',
+            "unstable_alpha_half.png")
+
+    # Translation Time vs. CDS
+    m_5, c_5 = get_fit(gene_data, "CDS", "T1_exp")
+    r_5, p_5 = get_correlation(gene_data, "CDS", "T1_exp")
+    plotter(gene_data, m_5, c_5, r_5, p_5, "CDS",
+            "T1_exp", 'CDS Length (codons)', 'Mean Translation Time (s)', 'Translation Time vs. CDS',
+            "unstable_t1_cds.png")
+
+    # Translation Time vs. Initiation Time.
+    m_6, c_6 = get_fit(gene_data, "lambda", "T1_exp")
+    r_6, p_6 = get_correlation(gene_data, "lambda", "T1_exp")
+    plotter(gene_data, m_6, c_6, r_6, p_6, "lambda",
+            "T1_exp", 'Initiation Time ' +
+            r'[$\lambda$] ', 'Mean Translation Time (s)', 'Translation Time vs. Initiation Time',
+            "unstable_t1_lambda.png")
+        
+    # Translation Time vs. Initiation Rate.
+    m_7, c_7 = get_fit(gene_data, "alpha", "T1_exp")
+    r_7, p_7 = get_correlation(gene_data, "alpha", "T1_exp")
+    plotter(gene_data, m_7, c_7, r_7, p_7, "alpha",
+            "T1_exp", 'Initiation Rate ' +
+            r'[$\alpha$] $s^{-1}$', 'Mean Translation Time (s)', 'Translation Time vs. Initiation Rate',
+            "unstable_t1_alpha.png")
+    
+    # Translation Time vs. CDS
+    m_8, c_8 = get_fit(gene_data, "CDS", "alpha")
+    r_8, p_8 = get_correlation(gene_data, "CDS", "alpha")
+    plotter(gene_data, m_8, c_8, r_8, p_8, "CDS",
+            "alpha", 'CDS Length (codons)', r'[$\alpha$] $s^{-1}$', 'Initiation Rate vs. CDS',
+            "unstable_alpha_cds.png")
+    
 main()
