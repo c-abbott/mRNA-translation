87ead842a843f33fbed946abf6be4aac00679b8a
c-abbott <callum.abbott@outlook.com>
2020-03-13 11:43:27 +0000

    Added plotters for steady state regime.

--- ProteinSynthesis.py
+++ ProteinSynthesis.py
@@ -14,6 +14,7 @@ import random
 import math
 import matplotlib.pyplot as plt
 
+
 class ProteinSynthesis(object):
     """
         A class to simulate protein synthesis. Specifically the
@@ -74,10 +75,10 @@ class ProteinSynthesis(object):
         sum = self.a[0]
         j = 0
         while sum < r:
-            sum += self.a[j+1]
+            sum += self.a[j + 1]
             j += 1
         return j
-    
+
     def update(self, index):
         """
             Update method to realise the transition
@@ -88,92 +89,92 @@ class ProteinSynthesis(object):
             if index == 0:
                 self.taus[1] = 1
                 self.a[0] = 0
-                self.a[1] = self.omegas[1]*(1 - self.taus[self.length+1])
+                self.a[1] = self.omegas[1] * (1 - self.taus[self.length + 1])
 
             # Elongation at index.
-            elif index >= 1 and index <= self.length-1:
+            elif index >= 1 and index <= self.length - 1:
                 self.taus[index] = 0
-                self.taus[index+1] = 1
+                self.taus[index + 1] = 1
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1] * \
-                    (1-self.taus[index+1+self.length])
+                self.a[index + 1] = self.omegas[index + 1] * \
+                    (1 - self.taus[index + 1 + self.length])
 
             # Elongation at index and intiation.
             elif index == (self.length):
                 self.taus[index] = 0
-                self.taus[index+1] = 1
+                self.taus[index + 1] = 1
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1] * \
-                    (1-self.taus[index+1+self.length])
+                self.a[index + 1] = self.omegas[index + 1] * \
+                    (1 - self.taus[index + 1 + self.length])
                 self.a[0] = self.alpha
 
             # Elongation and potential unblocking.
-            elif index >= (self.length + 1) and index <= (self.size-self.length-2):
+            elif index >= (self.length + 1) and index <= (self.size - self.length - 2):
                 self.taus[index] = 0
-                self.taus[index+1] = 1
+                self.taus[index + 1] = 1
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1] * \
-                    (1-self.taus[index+1+self.length])
-                self.a[index-self.length] = self.omegas[index - self.length] \
-                                            * self.taus[index - self.length]
+                self.a[index + 1] = self.omegas[index + 1] * \
+                    (1 - self.taus[index + 1 + self.length])
+                self.a[index - self.length] = self.omegas[index - self.length] \
+                    * self.taus[index - self.length]
 
             # No ribosomes ahead.
-            elif index >= (self.size-self.length-1) and index <= (self.size-2):
+            elif index >= (self.size - self.length - 1) and index <= (self.size - 2):
                 self.taus[index] = 0
-                self.taus[index+1] = 1
+                self.taus[index + 1] = 1
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1]
-                self.a[index-self.length] = self.omegas[index - self.length] \
-                                            * self.taus[index - self.length]
-                
+                self.a[index + 1] = self.omegas[index + 1]
+                self.a[index - self.length] = self.omegas[index - self.length] \
+                    * self.taus[index - self.length]
+
             # Detaching from lattice.
-            elif index == (self.size-1):
+            elif index == (self.size - 1):
                 self.taus[index] = 0
                 self.a[index] = 0
-                self.a[index-self.length] = self.omegas[index - self.length] \
-                                            * self.taus[index - self.length]
+                self.a[index - self.length] = self.omegas[index - self.length] \
+                    * self.taus[index - self.length]
         else:
             # Initiation.
             if index == 0:
-                self.taus[1] = 1 # Site 2.
+                self.taus[1] = 1  # Site 2.
                 self.a[0] = 0
-                self.a[1] = self.omegas[1]*(1 - self.taus[self.length+1])
+                self.a[1] = self.omegas[1] * (1 - self.taus[self.length + 1])
 
             # Elongation and Initiation
             if index == self.length:
                 self.taus[index] = 0    # Site 2.
-                self.taus[index+1] = 1  # Site 3.
+                self.taus[index + 1] = 1  # Site 3.
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1] * \
-                    (1-self.taus[index+1+self.length])
+                self.a[index + 1] = self.omegas[index + 1] * \
+                    (1 - self.taus[index + 1 + self.length])
                 self.a[0] = self.alpha
 
             # Elongation and potential unblocking.
-            elif index >= (self.length + 1) and index <= (self.size-self.length-2):
+            elif index >= (self.length + 1) and index <= (self.size - self.length - 2):
                 self.taus[index] = 0
-                self.taus[index+1] = 1
+                self.taus[index + 1] = 1
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1] * \
-                    (1-self.taus[index+1+self.length])
-                self.a[index-self.length] = self.omegas[index - self.length] \
+                self.a[index + 1] = self.omegas[index + 1] * \
+                    (1 - self.taus[index + 1 + self.length])
+                self.a[index - self.length] = self.omegas[index - self.length] \
                     * self.taus[index - self.length]
 
             # No ribosomes ahead.
-            elif index >= (self.size-self.length-1) and index <= (self.size-2):
+            elif index >= (self.size - self.length - 1) and index <= (self.size - 2):
                 self.taus[index] = 0
-                self.taus[index+1] = 1
+                self.taus[index + 1] = 1
                 self.a[index] = 0
-                self.a[index+1] = self.omegas[index+1]
-                self.a[index-self.length] = self.omegas[index - self.length] \
+                self.a[index + 1] = self.omegas[index + 1]
+                self.a[index - self.length] = self.omegas[index - self.length] \
                     * self.taus[index - self.length]
 
             # Detaching from lattice.
-            elif index == (self.size-1):
+            elif index == (self.size - 1):
                 self.taus[index] = 0
                 self.a[index] = 0
-                self.a[index-self.length] = self.omegas[index - self.length] \
+                self.a[index - self.length] = self.omegas[index - self.length] \
                     * self.taus[index - self.length]
-        
+
     def get_occupation_number(self):
         """
             A method to determine the total number
@@ -186,11 +187,11 @@ class ProteinSynthesis(object):
             A method to determine the waiting time
             for the system to reach a steady state.
        """
-        if (occ_num_new - occ_num_old) <= tolerance*occ_num_old:
+        if (occ_num_new - occ_num_old) <= tolerance * occ_num_old:
             return True
         else:
             return False
-    
+
     def get_densities(self, state, t1, t0):
         """
             A method to calculate the unormalised probability
@@ -198,7 +199,7 @@ class ProteinSynthesis(object):
         """
         densities = state * (t1 - t0)
         return densities
-    
+
     def plot_density(self, x_data, y_data, t_1, gene):
         """
             Density plotter for mRNA strand.
@@ -208,11 +209,23 @@ class ProteinSynthesis(object):
         plt.xlabel("Time (s)")
         plt.ylabel(r"Denisty [$\rho$]")
         plt.plot(x_data, y_data)
-        plt.axvline(x = t_1, color = 'r', linestyle = '--')
-        plt.axhline(y = y_data.max(), color = 'b', linestyle = '--')
+        plt.axvline(x=t_1, color='r', linestyle='--')
+        plt.axhline(y=y_data.max(), color='b', linestyle='--')
         plt.savefig("density_plots/" + gene + "_plot.png")
         plt.show()
-    
+
+    def plot_ss_density(self, x_data, y_data):
+        """
+            Density plotter for mRNA strand in
+            steady state.
+        """
+        plt.title("Lattice Site Density")
+        plt.xlabel("Time (s)")
+        plt.ylabel(r"Denisty [$\rho$]")
+        #plt.ylim(0.0, 1.0)
+        plt.plot(x_data, y_data)
+        plt.show()
+
     def plot_current(self, x_data, y_data):
         """
             Current plotter for mRNA strand.
@@ -220,22 +233,23 @@ class ProteinSynthesis(object):
         plt.title("Lattice Site Current")
         plt.xlabel("Lattice Site")
         plt.ylabel("Current [J]")
+        #plt.ylim(0.0, 1.0)
         plt.plot(x_data, y_data)
         plt.show()
-    
+
     def save_data(self, x_data, y_data, gene):
         """
             Gene dependent data file writing.
         """
         with open(gene + "_density_data.dat", "w+") as f:
             f.writelines(map("{}, {}\n".format, x_data, y_data))
-    
+
     def get_k1(self, t, dt):
         """
             Method to find lower tick.
         """
         return (math.floor(t / dt) + 1)
-    
+
     def get_k2(self, t, dt):
         """
             Method to find upper tick.
